6baa9684ce5ccf1eeca79c5fbc1b3e8efcafb5ac
jonsan <gaigaisang@outlook.com>
2023-11-22 20:05:26 +0800

    up clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,13 +31,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "jug24j9fyn");
-        </script>
+      <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "jug24j9fyn");
+            `,
+          }}
+        />
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
