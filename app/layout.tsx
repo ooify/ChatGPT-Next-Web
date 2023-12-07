a8126bbb2f9ce70c76cfd12e44be73b7a8e6609a
jonsan <gaigaisang@outlook.com>
2023-12-07 14:32:11 +0800

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
               })(window, document, "clarity", "script", "jug24j9fyn");
             `,
             
-            
           }}
         />
         <meta name="config" content={JSON.stringify(getClientConfig())} />
